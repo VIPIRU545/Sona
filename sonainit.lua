233868110683bc37b6f9236ef63b3ba5a6294694
VIPIRU545 <60122748+VIPIRU545@users.noreply.github.com>
2020-03-11 16:32:54 -0400

    Delete sonainit.lua

--- sonainit.lua
+++
@@ -1,8 +0,0 @@
-getgenv().require = function(Module)
-local storecontext = getthreadcontext() 
-setthreadcontext(2)
-local truthvalue, Mod = pcall(getrenv().require, Module)
-setthreadcontext(storecontext)
-if not truthvalue then error("Failure 1!") end 
-return Mod 
-end
